77dd189531b6a6b8c98494c15bb553afa1a225e6
NandanKumar25CSE <2025cs_chilukurinandankumar_b@nie.ac.in>
2026-09-21 14:49:34 +0530

    decorator named my decorator is creaated and is invoked

---
+++ DAY/P5.py
@@ -0,0 +1,12 @@
+#concept: decorator
+#decorators are the functions that modify the functionality of another function.
+def my_decorator(func):
+    def wrapper():
+        print("before");
+        func()
+        print("after");
+    return wrapper
+@my_decorator
+def say_hello():
+    print("Hello!")
+say_hello() 
