51f883856818b88f6bcd50096a2fae0a891ac181
NandanKumar25CSE <2025cs_chilukurinandankumar_b@nie.ac.in>
2026-09-22 15:10:48 +0530

    #Pydantic model

--- DAY/DAY 2/FASTAPI_practice/main.py
+++ DAY/DAY 2/FASTAPI_practice/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 app = FastAPI()
 @app.get("/")
 def read_root():
@@ -27,3 +28,17 @@ def get_result(usn ):
 @app.get("/candidate/{roll}")
 def get_candidate(roll : int):
     return{"roll": roll, "type":str(type(roll)), "Result": "passed"}
+
+
+
+#Pydantic model
+class Item(BaseModel):
+    name : str
+    price : float
+    in_stock : bool = True
+
+@app.post("/items/")
+
+def create_item(item: Item):
+        return {"received_item": item, "total_price": item.price * 1.18, "message": "Item created successfully"
+        ""}
