ee7d1fe7132d1bfb39b49638a8deef32c773d37b
NandanKumar25CSE <2025cs_chilukurinandankumar_b@nie.ac.in>
2026-09-22 13:15:30 +0530

    path parameters adding

--- DAY/DAY 2/FASTAPI_practice/main.py
+++ DAY/DAY 2/FASTAPI_practice/main.py
@@ -11,8 +11,14 @@ def health():
     return {"status": "healthy"}
 
 
+
 #POST request
 @app.post("/create")
 def create_something(item : dict):
 
     return {"message" : "Item created succesfully", "item" : item  }
+
+#Path Parameters
+@app.get("/student/{usn}")
+def get_result(usn ):
+    return{"usn": usn, "Result": "passed"}
